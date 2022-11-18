448377cd9d06a3b87a507c039a41062f26e8ff37
john-reiner <johnreiner1990@gmail.com>
2022-11-18 11:10:15 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ruby:3.1.2
+
+RUN apt-get update -qq && apt-get install -y postgresql-client
+# throw errors if Gemfile has been modified since Gemfile.lock
+RUN bundle config --global frozen 1
+
+WORKDIR /app
+
+COPY . .
+RUN bundle install
+RUN chmod +x entrypoint.sh
+ENTRYPOINT ["./entrypoint.sh"]
+EXPOSE 3000
+
+# Start the main process.
+CMD ["rails", "server", "-b", "0.0.0.0"]
---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+require 'faker'
+
+Author.delete_all
+Article.delete_all
+
+10.times do
+    Author.create(name: Faker::Book.unique.author)
+end
+
+50.times do
+    Article.create({
+        title: Faker::Book.title,
+        body: Faker::Lorem.paragraphs(number: rand(5..7)),
+        author: Author.limit(1).order('RANDOM()').first # sql random
+    })
+end
